d8db27e70dad0aace6ce1ae428805388fa2cc0d4
0xKona <konarobinson@outlook.com>
2026-03-11 12:04:14 +0000

    add/cognito-user-pool

--- packages/backend/lib/stacks/auth-stack.ts
+++ packages/backend/lib/stacks/auth-stack.ts
@@ -1,16 +1,29 @@
+import { aws_cognito } from 'aws-cdk-lib';
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AuthStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // UserPool - Stores User Data
+    const userPool = new aws_cognito.UserPool(this, "Uni-Verse-Cognito-Pool", {
+      userPoolName: "UniVerseUserPool",
+      selfSignUpEnabled: true,
+      autoVerify: {
+        email: true, // Ask Users to Confirm sign up via email
+      },
+      // Verify and recover accounts via a OTP
+      accountRecovery: aws_cognito.AccountRecovery.EMAIL_ONLY,
+      userVerification: {
+        emailStyle: aws_cognito.VerificationEmailStyle.CODE,
+      },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true,
+        },
+      },
+    });
   }
 }
