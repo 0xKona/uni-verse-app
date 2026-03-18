c01cee236ffe9d826574992cb36d4ecf70bd77fa
0xKona <konarobinson@outlook.com>
2026-03-18 12:20:49 +0000

    feat/client-auth-system

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import AmplifyProvider from "@/components/AmplifyProvider";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AmplifyProvider>{children}</AmplifyProvider>
       </body>
     </html>
   );
